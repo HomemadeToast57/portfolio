9060d0b85d5a93bbf69ed472e34d927466aff370
HomemadeToast <54961512+HomemadeToast57@users.noreply.github.com>
2021-10-20 17:46:49 -0400

    Added darkmode switch with animation.

--- resources/scripts/darkmode.js
+++ resources/scripts/darkmode.js
@@ -1,26 +1,52 @@
 const html = document.getElementById("html");
 const logo = document.getElementById("logo");
 const button = document.getElementById("dark-mode-button");
+
 var dark = false;
 
-function darkmode() {
- if (dark) {
-   html.classList.remove("light-mode");
-   html.classList.add("dark-mode");
-   logo.setAttribute("src", "images/logo-light.png");
- } else {
-   html.classList.remove("dark-mode");
-   html.classList.add("light-mode");
-   logo.setAttribute("src", "images/logo-dark.png");
- }
+var darkString = window.localStorage.getItem("light-mode");
+
+function initLightMode() {
+  if (darkString != null) {
+    if (darkString == "light") {
+      dark = false;
+    } else {
+      switcher();
+      dark = true;
+    }
+  }
+  darkmode();
 }
 
-darkmode();
+function darkmode() {
+  if (dark) {
+    html.classList.remove("light-mode");
+    html.classList.add("dark-mode");
+    logo.setAttribute("src", "images/logo-light.png");
+    window.localStorage.setItem("light-mode", "dark");
+  } else {
+    html.classList.remove("dark-mode");
+    html.classList.add("light-mode");
+    logo.setAttribute("src", "images/logo-dark.png");
+    window.localStorage.setItem("light-mode", "light");
+  }
+}
+
+function switcher() {
+  dark = !dark;
+  const sun = document.querySelector(".sun");
+  const moon = document.querySelector(".moon");
+  const button = document.querySelector(".container");
+
+    sun.classList.toggle("visible");
+    moon.classList.toggle("visible");
+}
 
 button.onclick = function () {
-  dark = !dark;
+  
+  switcher();  
   darkmode();
-};  
+  
+};
 
-
-
+initLightMode();
