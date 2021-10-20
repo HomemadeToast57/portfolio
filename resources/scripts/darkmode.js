3f7510813c40d29c237d47ae9f4fdecacc9dd507
HomemadeToast <54961512+HomemadeToast57@users.noreply.github.com>
2021-10-20 12:16:21 -0400

    Added dark mode button.

--- resources/scripts/darkmode.js
+++ resources/scripts/darkmode.js
@@ -1,11 +1,26 @@
 const html = document.getElementById("html");
 const logo = document.getElementById("logo");
+const button = document.getElementById("dark-mode-button");
 var dark = false;
 
-if (dark) {
-    html.classList.add("dark-mode");
-    logo.setAttribute("src", "images/logo-light.png")
-} else {
-    html.classList.add("light-mode");
-    logo.setAttribute("src", "images/logo-dark.png");
+function darkmode() {
+ if (dark) {
+   html.classList.remove("light-mode");
+   html.classList.add("dark-mode");
+   logo.setAttribute("src", "images/logo-light.png");
+ } else {
+   html.classList.remove("dark-mode");
+   html.classList.add("light-mode");
+   logo.setAttribute("src", "images/logo-dark.png");
+ }
 }
+
+darkmode();
+
+button.onclick = function () {
+  dark = !dark;
+  darkmode();
+};  
+
+
+
